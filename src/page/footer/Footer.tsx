e18abb70c363f006596bc4d32eff6f31717bce9e
ptdrpg <pagielamizael@gmail.com>
2023-11-21 16:04:51 +0300

    refixing font

--- src/page/footer/Footer.tsx
+++ src/page/footer/Footer.tsx
@@ -4,7 +4,7 @@ import'./footer.scss'
 
 const Footer = () => {
   return (
-    <footer className="container footercontainer">
+    <footer className="footercontainer">
       <Text tag='h2' children="Andry Pagiela" classname="footerInitial" />
       <Text tag='p' children="Le lorem ipsum est, en imprimerie, une suite de mots sans signification utilisée à titre provisoire pour calibrer une mise en page, le texte définitif venant remplacer le faux-texte dès qu'il est prêt o" classname="footerDescription" />
       <SocialMedia />

1364db9fb332a8d93b2df2e0803465726cf3e1c9
ptdrpg <pagielamizael@gmail.com>
2023-11-22 16:09:12 +0300

    icon fixing

---
+++ src/component/icon/Facebook.tsx
@@ -0,0 +1,19 @@
+type Props = {}
+
+function Facebook({}: Props) {
+  return (
+    <svg width="50" height="50" viewBox="0 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg">
+<g clipPath="url(#clip0_79_44)">
+<path d="M14.5833 20.8333V29.1667H20.8333V43.75H29.1667V29.1667H35.4167L37.5 20.8333H29.1667V16.6667C29.1667 16.1141 29.3862 15.5842 29.7769 15.1935C30.1676 14.8028 30.6975 14.5833 31.25 14.5833H37.5V6.25H31.25C28.4873 6.25 25.8378 7.34747 23.8843 9.30097C21.9308 11.2545 20.8333 13.904 20.8333 16.6667V20.8333H14.5833Z" stroke="#53FC83" strokeWidth="2.75" strokeLinecap="round" strokeLinejoin="round"/>
+</g>
+<defs>
+<clipPath id="clip0_79_44">
+<rect width="50" height="50" fill="white"/>
+</clipPath>
+</defs>
+</svg>
+
+  )
+}
+
+export default Facebook
---
+++ src/component/icon/Linkedin.tsx
@@ -0,0 +1,23 @@
+type Props = {}
+
+function Linkedin({}: Props) {
+  return (
+    <svg width="50" height="50" viewBox="0 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg">
+<g clip-path="url(#clip0_79_52)">
+<path d="M8.33333 12.5002C8.33333 11.3951 8.77232 10.3353 9.55372 9.55388C10.3351 8.77248 11.3949 8.3335 12.5 8.3335H37.5C38.6051 8.3335 39.6649 8.77248 40.4463 9.55388C41.2277 10.3353 41.6667 11.3951 41.6667 12.5002V37.5002C41.6667 38.6052 41.2277 39.665 40.4463 40.4464C39.6649 41.2278 38.6051 41.6668 37.5 41.6668H12.5C11.3949 41.6668 10.3351 41.2278 9.55372 40.4464C8.77232 39.665 8.33333 38.6052 8.33333 37.5002V12.5002Z" stroke="#53FC83" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+<path d="M16.6667 22.9165V33.3332" stroke="#53FC83" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+<path d="M16.6667 16.6665V16.6873" stroke="#53FC83" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+<path d="M25 33.3332V22.9165" stroke="#53FC83" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+<path d="M33.3333 33.3332V27.0832C33.3333 25.9781 32.8943 24.9183 32.1129 24.1369C31.3315 23.3555 30.2717 22.9165 29.1667 22.9165C28.0616 22.9165 27.0018 23.3555 26.2204 24.1369C25.439 24.9183 25 25.9781 25 27.0832" stroke="#53FC83" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+</g>
+<defs>
+<clipPath id="clip0_79_52">
+<rect width="50" height="50" fill="white"/>
+</clipPath>
+</defs>
+</svg>
+
+  )
+}
+
+export default Linkedin

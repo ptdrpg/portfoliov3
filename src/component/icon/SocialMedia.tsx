c405afd581c17412d909390a5032c0255cdc777f
ptdrpg <pagielamizael@gmail.com>
2023-11-23 10:34:52 +0300

    about icon fix

--- src/component/icon/SocialMedia.tsx
+++ src/component/icon/SocialMedia.tsx
@@ -1,44 +1,52 @@
-
+import Facebook from "./Facebook"
+import Linkedin from "./Linkedin"
+import Instagram from "./Instagram"
+import Tweeter from "./Tweeter"
 
 type Props = {}
 
 function SocialMedia({}: Props) {
   return (
-    <svg width="250" height="70" viewBox="0 0 250 70" fill="none" xmlns="http://www.w3.org/2000/svg">
-<g clipPath="url(#clip0_9_198)">
-<path d="M24.5833 30.8333V39.1667H30.8333V53.75H39.1667V39.1667H45.4167L47.5 30.8333H39.1667V26.6667C39.1667 26.1141 39.3862 25.5842 39.7769 25.1935C40.1676 24.8028 40.6975 24.5833 41.25 24.5833H47.5V16.25H41.25C38.4873 16.25 35.8378 17.3475 33.8843 19.301C31.9308 21.2545 30.8333 23.904 30.8333 26.6667V30.8333H24.5833Z" stroke="#53FC83" strokeWidth="2.75" strokeLinecap="round" strokeLinejoin="round"/>
-</g>
-<g clipPath="url(#clip1_9_198)">
-<path d="M78.3333 26.6667C78.3333 24.4565 79.2113 22.3369 80.7741 20.7741C82.3369 19.2113 84.4565 18.3333 86.6667 18.3333H103.333C105.543 18.3333 107.663 19.2113 109.226 20.7741C110.789 22.3369 111.667 24.4565 111.667 26.6667V43.3333C111.667 45.5435 110.789 47.6631 109.226 49.2259C107.663 50.7887 105.543 51.6667 103.333 51.6667H86.6667C84.4565 51.6667 82.3369 50.7887 80.7741 49.2259C79.2113 47.6631 78.3333 45.5435 78.3333 43.3333V26.6667Z" stroke="#53FC83" strokeWidth="2.75" strokeLinecap="round" strokeLinejoin="round"/>
-<path d="M88.75 35C88.75 36.6576 89.4085 38.2473 90.5806 39.4194C91.7527 40.5915 93.3424 41.25 95 41.25C96.6576 41.25 98.2473 40.5915 99.4194 39.4194C100.592 38.2473 101.25 36.6576 101.25 35C101.25 33.3424 100.592 31.7527 99.4194 30.5806C98.2473 29.4085 96.6576 28.75 95 28.75C93.3424 28.75 91.7527 29.4085 90.5806 30.5806C89.4085 31.7527 88.75 33.3424 88.75 35Z" stroke="#53FC83" strokeWidth="2.75" strokeLinecap="round" strokeLinejoin="round"/>
-<path d="M104.375 25.625V25.6458" stroke="#53FC83" strokeWidth="2.75" strokeLinecap="round" strokeLinejoin="round"/>
-</g>
-<g clipPath="url(#clip2_9_198)">
-<path d="M138.333 22.5C138.333 21.3949 138.772 20.3351 139.554 19.5537C140.335 18.7723 141.395 18.3333 142.5 18.3333H167.5C168.605 18.3333 169.665 18.7723 170.446 19.5537C171.228 20.3351 171.667 21.3949 171.667 22.5V47.5C171.667 48.6051 171.228 49.6649 170.446 50.4463C169.665 51.2277 168.605 51.6667 167.5 51.6667H142.5C141.395 51.6667 140.335 51.2277 139.554 50.4463C138.772 49.6649 138.333 48.6051 138.333 47.5V22.5Z" stroke="#53FC83" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-<path d="M146.667 32.9167V43.3333" stroke="#53FC83" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-<path d="M146.667 26.6667V26.6875" stroke="#53FC83" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-<path d="M155 43.3333V32.9167" stroke="#53FC83" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-<path d="M163.333 43.3333V37.0833C163.333 35.9783 162.894 34.9184 162.113 34.137C161.332 33.3556 160.272 32.9167 159.167 32.9167C158.062 32.9167 157.002 33.3556 156.22 34.137C155.439 34.9184 155 35.9783 155 37.0833" stroke="#53FC83" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-</g>
-<g clipPath="url(#clip3_9_198)">
-<path d="M235.833 18.3542C233.75 19.375 231.708 19.7896 229.583 20.4167C227.248 17.7812 223.785 17.6354 220.458 18.8812C217.131 20.1271 214.952 23.1729 215 26.6667V28.75C208.24 28.9229 202.219 25.8437 198.333 20.4167C198.333 20.4167 189.621 35.9021 206.667 43.3333C202.767 45.9312 198.877 47.6833 194.167 47.5C201.058 51.2562 208.569 52.5479 215.071 50.6604C222.529 48.4937 228.658 42.9042 231.01 34.5312C231.712 31.9847 232.06 29.3539 232.046 26.7125C232.046 26.1937 235.192 20.9375 235.833 18.3521V18.3542Z" stroke="#53FC83" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-</g>
-<defs>
-<clipPath id="clip0_9_198">
-<rect width="50" height="50" fill="white" transform="translate(10 10)"/>
-</clipPath>
-<clipPath id="clip1_9_198">
-<rect width="50" height="50" fill="white" transform="translate(70 10)"/>
-</clipPath>
-<clipPath id="clip2_9_198">
-<rect width="50" height="50" fill="white" transform="translate(130 10)"/>
-</clipPath>
-<clipPath id="clip3_9_198">
-<rect width="50" height="50" fill="white" transform="translate(190 10)"/>
-</clipPath>
-</defs>
-</svg>
-
+//     <svg width="250" height="70" viewBox="0 0 250 70" fill="none" xmlns="http://www.w3.org/2000/svg">
+// <g clipPath="url(#clip0_9_198)">
+// <path d="M24.5833 30.8333V39.1667H30.8333V53.75H39.1667V39.1667H45.4167L47.5 30.8333H39.1667V26.6667C39.1667 26.1141 39.3862 25.5842 39.7769 25.1935C40.1676 24.8028 40.6975 24.5833 41.25 24.5833H47.5V16.25H41.25C38.4873 16.25 35.8378 17.3475 33.8843 19.301C31.9308 21.2545 30.8333 23.904 30.8333 26.6667V30.8333H24.5833Z" stroke="#53FC83" strokeWidth="2.75" strokeLinecap="round" strokeLinejoin="round"/>
+// </g>
+// <g clipPath="url(#clip1_9_198)">
+// <path d="M78.3333 26.6667C78.3333 24.4565 79.2113 22.3369 80.7741 20.7741C82.3369 19.2113 84.4565 18.3333 86.6667 18.3333H103.333C105.543 18.3333 107.663 19.2113 109.226 20.7741C110.789 22.3369 111.667 24.4565 111.667 26.6667V43.3333C111.667 45.5435 110.789 47.6631 109.226 49.2259C107.663 50.7887 105.543 51.6667 103.333 51.6667H86.6667C84.4565 51.6667 82.3369 50.7887 80.7741 49.2259C79.2113 47.6631 78.3333 45.5435 78.3333 43.3333V26.6667Z" stroke="#53FC83" strokeWidth="2.75" strokeLinecap="round" strokeLinejoin="round"/>
+// <path d="M88.75 35C88.75 36.6576 89.4085 38.2473 90.5806 39.4194C91.7527 40.5915 93.3424 41.25 95 41.25C96.6576 41.25 98.2473 40.5915 99.4194 39.4194C100.592 38.2473 101.25 36.6576 101.25 35C101.25 33.3424 100.592 31.7527 99.4194 30.5806C98.2473 29.4085 96.6576 28.75 95 28.75C93.3424 28.75 91.7527 29.4085 90.5806 30.5806C89.4085 31.7527 88.75 33.3424 88.75 35Z" stroke="#53FC83" strokeWidth="2.75" strokeLinecap="round" strokeLinejoin="round"/>
+// <path d="M104.375 25.625V25.6458" stroke="#53FC83" strokeWidth="2.75" strokeLinecap="round" strokeLinejoin="round"/>
+// </g>
+// <g clipPath="url(#clip2_9_198)">
+// <path d="M138.333 22.5C138.333 21.3949 138.772 20.3351 139.554 19.5537C140.335 18.7723 141.395 18.3333 142.5 18.3333H167.5C168.605 18.3333 169.665 18.7723 170.446 19.5537C171.228 20.3351 171.667 21.3949 171.667 22.5V47.5C171.667 48.6051 171.228 49.6649 170.446 50.4463C169.665 51.2277 168.605 51.6667 167.5 51.6667H142.5C141.395 51.6667 140.335 51.2277 139.554 50.4463C138.772 49.6649 138.333 48.6051 138.333 47.5V22.5Z" stroke="#53FC83" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+// <path d="M146.667 32.9167V43.3333" stroke="#53FC83" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+// <path d="M146.667 26.6667V26.6875" stroke="#53FC83" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+// <path d="M155 43.3333V32.9167" stroke="#53FC83" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+// <path d="M163.333 43.3333V37.0833C163.333 35.9783 162.894 34.9184 162.113 34.137C161.332 33.3556 160.272 32.9167 159.167 32.9167C158.062 32.9167 157.002 33.3556 156.22 34.137C155.439 34.9184 155 35.9783 155 37.0833" stroke="#53FC83" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+// </g>
+// <g clipPath="url(#clip3_9_198)">
+// <path d="M235.833 18.3542C233.75 19.375 231.708 19.7896 229.583 20.4167C227.248 17.7812 223.785 17.6354 220.458 18.8812C217.131 20.1271 214.952 23.1729 215 26.6667V28.75C208.24 28.9229 202.219 25.8437 198.333 20.4167C198.333 20.4167 189.621 35.9021 206.667 43.3333C202.767 45.9312 198.877 47.6833 194.167 47.5C201.058 51.2562 208.569 52.5479 215.071 50.6604C222.529 48.4937 228.658 42.9042 231.01 34.5312C231.712 31.9847 232.06 29.3539 232.046 26.7125C232.046 26.1937 235.192 20.9375 235.833 18.3521V18.3542Z" stroke="#53FC83" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+// </g>
+// <defs>
+// <clipPath id="clip0_9_198">
+// <rect width="50" height="50" fill="white" transform="translate(10 10)"/>
+// </clipPath>
+// <clipPath id="clip1_9_198">
+// <rect width="50" height="50" fill="white" transform="translate(70 10)"/>
+// </clipPath>
+// <clipPath id="clip2_9_198">
+// <rect width="50" height="50" fill="white" transform="translate(130 10)"/>
+// </clipPath>
+// <clipPath id="clip3_9_198">
+// <rect width="50" height="50" fill="white" transform="translate(190 10)"/>
+// </clipPath>
+// </defs>
+// </svg>
+<div className="footer-social">
+        <a href="https://www.facebook.com/andry.pagiela.5"><Facebook /></a>
+        <a href="https://www.instagram.com/pagiela_mizael/"><Instagram /></a>
+        <a href=""><Linkedin /></a>
+        <a href=""><Tweeter /></a>
+      </div>
   )
 }
 
